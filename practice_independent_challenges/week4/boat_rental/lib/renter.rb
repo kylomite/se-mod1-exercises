fcfdb20ebc77e5ddc5b9766533715fa5a757fd33
Kyle Delaney <khdelaney99@gmail.com>
2024-07-24 09:00:16 -0400

    add test for #initialize method for Renter

--- practice_independent_challenges/week4/boat_rental/lib/renter.rb
+++ practice_independent_challenges/week4/boat_rental/lib/renter.rb
@@ -1,1 +1,5 @@
-
+class Renter
+    attr_reader :name,
+                :credit_card_number
+    
+end
--- practice_independent_challenges/week4/boat_rental/spec/renter_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/renter_spec.rb
@@ -3,3 +3,21 @@ require 'spec_helper'
 RSpec.configure do |config|
   config.formatter = :documentation
 end
+
+RSpec.describe Renter do 
+    before(:each) do 
+       @renter = Renter.new("Monkey D Luffy", "1738173817381738")
+    end
+
+    describe '#initialize()' do
+        it 'creates an instance of a Renter object' do
+            expect(@renter).to be_a (Renter)
+        end
+        it 'has a name attribute' do
+
+        end
+        it 'has a credit_card_number attribute' do
+            
+        end
+    end
+end
