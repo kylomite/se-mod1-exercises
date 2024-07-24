51118cf19a6a77b1f8d9cddf851c3e864b92fd00
Kyle Delaney <khdelaney99@gmail.com>
2024-07-24 09:34:15 -0400

    pass initial test for #rent() method in Dock

--- practice_independent_challenges/week4/boat_rental/lib/dock.rb
+++ practice_independent_challenges/week4/boat_rental/lib/dock.rb
@@ -4,5 +4,14 @@ class Dock
     def initialize(name, max_rental_time)
         @name = name
         @max_rental_time = max_rental_time
+        @rental_log = Hash.new(0)
+    end
+
+    def rent(boat, renter)
+        @rental_log[boat] = renter
+    end
+
+    def rental_log
+        @rental_log
     end
 end
--- practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
+++ practice_independent_challenges/week4/boat_rental/spec/dock_spec.rb
@@ -7,6 +7,11 @@ end
 RSpec.describe Dock do
     before(:each) do 
         @dock = Dock.new("Dock A", 4)
+        @kayak_1 = Boat.new(:kayak, 20)
+        @kayak_2 = Boat.new(:kayak, 20)
+        @Luffy = Renter.new("Monkey D Luffy", "1738173817381738")
+        @Zoro = Renter.new("Roanoa Zoro", "2407240724072407")
+
      end
 
      describe '#initialize()' do
@@ -17,7 +22,32 @@ RSpec.describe Dock do
             expect(@dock.name).to eq ("Dock A")
         end
         it 'has a max_rental_time attribute' do
-             expect(@dock.max_rental_time).to eq 4
+             expect(@dock.max_rental_time).to eq (4)
         end
+        it 'has a rental_log attribute' do
+            expect(@dock.rental_log.empty?()).to eq (true)
+        end
+     end
+
+     describe '#rental_log' do
+        it 'returns a rental_log hash which contains boats and renters' do
+            expect(@dock.rental_log.empty?()).to eq (true)
+            @dock.rent(@kayak_1, @Luffy)
+
+            expect(@dock.rental_log.empty?()).to eq (false)
+        end
+     end
+
+     describe '#rent()' do 
+        it 'will add an entry to the rental_log based on the arguments' do
+            expect(@dock.rental_log.empty?).to eq (true)
+            @dock.rent(@kayak_1, @Luffy)
+
+            expect(@dock.rental_log.empty?).to eq (false)
+            @dock.rent(@kayak_2, @Zoro)
+            expect(@dock.rental_log.empty?).to eq (false)
+            expect(@dock.rental_log.keys.length).to eq (2)
+        end
+     
      end
 end
