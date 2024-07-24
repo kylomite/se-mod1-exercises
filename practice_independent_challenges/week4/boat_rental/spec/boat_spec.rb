3d74a7dfe7344ee28bd4b229d40a1745ef25d349
Kyle Delaney <khdelaney99@gmail.com>
2024-07-24 08:43:28 -0400

    start files for boat and class, add test for #instantiate() for boat

---
+++ practice_independent_challenges/week4/boat_rental/spec/boat_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Boat do 
+    before(:each) do 
+       @kayak = Boat.new(:kayak, 20)
+    end
+
+    describe '#initialize' do
+        it 'will create an instance of a Boat object' do
+            expect(@kayak).to be_a (Boat)
+        end
+        it 'has a symbol value showing its type' do
+            expect(@kayak.type).to eq (:kayak)
+        end
+        it 'has a price per hour' do
+            expect(@kayak).to eq (20)
+        end
+    end
+end
