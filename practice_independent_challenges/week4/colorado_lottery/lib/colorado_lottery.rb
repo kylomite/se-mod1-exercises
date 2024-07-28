d90cea72cdab1da70d54ca2af5c6c7b36d9f76ea
Kyle Delaney <khdelaney99@gmail.com>
2024-07-28 19:08:57 -0400

    pass tests for #interested_and_18? in colorado_lottery

--- practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
@@ -6,4 +6,17 @@ class Coloradolottery
         @winners = []
         @current_contestants = {}
     end
+
+    def interested_and_18?(person, game)
+        #person.age >= 18 && person.game_interests.include?(game) ? true : false
+        if person.age >= 18 && person.game_interests.include?(game.name)
+            true
+        else 
+            false
+        end
+    end
+
+    def can_register?(name, game)
+
+    end
 end
