cfc9e349dbbe489ccfba0746f7a108eccc5fe27a
Kyle Delaney <khdelaney99@gmail.com>
2024-07-28 19:19:19 -0400

    pass tests for #can_register() method for colorado_lottery

--- practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
+++ practice_independent_challenges/week4/colorado_lottery/lib/colorado_lottery.rb
@@ -8,15 +8,19 @@ class Coloradolottery
     end
 
     def interested_and_18?(person, game)
-        #person.age >= 18 && person.game_interests.include?(game) ? true : false
-        if person.age >= 18 && person.game_interests.include?(game.name)
-            true
-        else 
-            false
-        end
+        person.age >= 18 && person.game_interests.include?(game.name) ?
+        true : false
     end
 
     def can_register?(name, game)
-
+        if interested_and_18?(name, game) == true
+            if game.national_drawing? == true
+                true
+            else 
+                true if name.state_of_residence == "CO"
+            end
+        else 
+            false
+        end
     end
 end
