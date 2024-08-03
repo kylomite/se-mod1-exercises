c5f9a40a1132a3e98325a03c6f3875c782ea944f
Kyle Delaney <khdelaney99@gmail.com>
2024-08-03 08:44:22 -0400

    add test for #calculate_total_owed in reunion

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Activity do
         end
     end
 
-    describe 'add_activity()' do
+    describe '#add_activity()' do
         it 'adds argument to activities array' do
             activity_1 = Activity.new("Brunch")
             @reunion.add_activity(activity_1)
@@ -27,7 +27,7 @@ RSpec.describe Activity do
         end
     end
 
-    describe 'total_reunion_cost' do
+    describe '#total_reunion_cost' do
         it 'returns the total cost of all activites in the event' do
             activity_1 = Activity.new("Brunch")
             activity_1.add_participant("Luffy", 15)
@@ -53,5 +53,44 @@ RSpec.describe Activity do
 
             expect(@reunion.total_reunion_cost).to eq(268)
         end
+    end
+
+    describe '#calculate_total_owed' do
+        it 'returns a hash of KVPs with each key being a participant name, and its value equaling the total due related to them' do
+            activity_1 = Activity.new("Brunch")
+            activity_1.add_participant("Luffy", 15)
+            activity_1.add_participant("Zoro", 1)
+            activity_1.add_participant("Nami", 50)
+            activity_1.add_participant("Usopp", 15)
+
+            activity_2 = Activity.new("Put-put")
+            activity_2.add_participant("Luffy", 10)
+            activity_2.add_participant("Zoro", 0)
+            activity_2.add_participant("Nami", 75)
+            activity_2.add_participant("Usopp", 12)
+
+            activity_3 = Activity.new("Bounce House")
+            activity_3.add_participant("Luffy", 20)
+            activity_3.add_participant("Zoro", 10)
+            activity_3.add_participant("Nami", 35)
+            activity_3.add_participant("Usopp", 25)
+
+            @reunion.add_activity(activity_1)
+            @reunion.add_activity(activity_2)
+            @reunion.add_activity(activity_3)
+
+            expected = {
+                        "Luffy" => 21,
+                        "Zoro" => 55,
+                        "Nami" => -94,
+                        "Usopp" => 14
+                    }
+
+            expect(@reunion.calculate_total_owed).to eq(expected)
+        end
+    end
+
+    describe '#print_participants_dues' do
+
     end
 end
