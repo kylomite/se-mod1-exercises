c01dd3d05791a5f047b3c199c17645ed514fdcb1
Kyle Delaney <khdelaney99@gmail.com>
2024-08-03 08:25:25 -0400

    add test for #total_cost in reunion

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -26,4 +26,32 @@ RSpec.describe Activity do
             expect(@reunion.activities).to eq([activity_1])
         end
     end
+
+    describe 'total_cost' do
+        it 'returns the total cost of all activites in the event' do
+            activity_1 = Activity.new("Brunch")
+            activity_1.add_participant("Luffy", 15)
+            activity_1.add_participant("Zoro", 1)
+            activity_1.add_participant("Nami", 50)
+            activity_1.add_participant("Usopp", 15)
+
+            activity_2 = Activity.new("Put-put")
+            activity_1.add_participant("Luffy", 10)
+            activity_1.add_participant("Zoro", 0)
+            activity_1.add_participant("Nami", 75)
+            activity_1.add_participant("Usopp", 12)
+
+            activity_3 = Activity.new("Bounce House")
+            activity_1.add_participant("Luffy", 20)
+            activity_1.add_participant("Zoro", 10)
+            activity_1.add_participant("Nami", 35)
+            activity_1.add_participant("Usopp", 25)
+
+            @reunion.add_activity(activity_1)
+            @reunion.add_activity(activity_2)
+            @reunion.add_activity(activity_3)
+
+            expect(@reunion.total_cost).to eq(268)
+        end
+    end
 end
