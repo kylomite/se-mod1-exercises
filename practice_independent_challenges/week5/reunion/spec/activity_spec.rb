bcb1f1cce8913c81d63b8c2c009829cfa14bfdcb
Kyle Delaney <khdelaney99@gmail.com>
2024-08-03 07:13:33 -0400

    add test for #initilaize() in activity

---
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -0,0 +1,20 @@
+require './spec/spec_helper'
+
+Rspec.describe Activity do
+    before(:each) do
+        @activity = Activity.new("Brunch")    
+    end
+
+    describe '#initialize()' do
+        it 'exists' do
+            expect(@activity).to be_an(Activity)
+        end
+        it 'has a name' do
+            expect(@activity.name).to eq('Brunch')
+        end
+
+        it 'has a participants hash' do
+            expect(@activity.participants).to eq({})
+        end
+    end
+end
